c5369dd915c22c7c9a41ce73d6d5926286d1c80d
Daniel Gilbert <ich@danielgilbert.de>
2017-03-18 22:28:13 +0100

    Add ViewModels

--- HistoryForSpotify/Views/HistoryListView.xaml.cs
+++ HistoryForSpotify/Views/HistoryListView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HistoryForSpotify.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,14 @@ namespace HistoryForSpotify.Views
     /// </summary>
     public partial class HistoryListView : UserControl
     {
+        private HistoryListViewModel _historyListViewModel;
+
         public HistoryListView()
         {
             InitializeComponent();
+
+            _historyListViewModel = new HistoryListViewModel();
+            this.DataContext = _historyListViewModel;
         }
     }
 }
--- HistoryForSpotify/Views/WaitForSpotifyView.xaml.cs
+++ HistoryForSpotify/Views/WaitForSpotifyView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HistoryForSpotify.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,14 @@ namespace HistoryForSpotify.Views
     /// </summary>
     public partial class WaitForSpotifyView : UserControl
     {
+        private WaitForSpotifyViewModel _waitForSpotifyViewModel;
+
         public WaitForSpotifyView()
         {
             InitializeComponent();
+
+            _waitForSpotifyViewModel = new WaitForSpotifyViewModel();
+            this.DataContext = _waitForSpotifyViewModel;
         }
     }
 }
