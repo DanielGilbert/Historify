193960228ced49625ba4a890b13b8c01bbf576ea
Daniel Gilbert <ich@danielgilbert.de>
2017-03-19 00:28:24 +0100

    Adds SpotifyAudioService

---
+++ HistoryForSpotify.Core/AudioServices/Interfaces/IAudioService.cs
@@ -0,0 +1,21 @@
+﻿using HistoryForSpotify.Core.AudioServices.Delegates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HistoryForSpotify.Core.AudioServices.Interfaces
+{
+    public interface IAudioService
+    {
+        event NewTrackDelegate OnNewSong;
+        event NewTrackTimeDelegate OnNewTrackTime;
+        event ServiceConnectedDelegate OnServiceConnected;
+        event ServiceDisconnectedDelegate OnServiceDisconnected;
+
+        string Name { get; }
+        void Connect();
+        void Disconnect();
+    }
+}
---
+++ HistoryForSpotify.Core/AudioServices/SpotifyAudioService.cs
@@ -0,0 +1,36 @@
+﻿using HistoryForSpotify.Core.AudioServices.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HistoryForSpotify.Core.AudioServices.Delegates;
+
+namespace HistoryForSpotify.Core.AudioServices
+{
+    public class SpotifyAudioService : IAudioService
+    {
+        public string Name
+        {
+            get
+            {
+                return "Spotify";
+            }
+        }
+
+        public event NewTrackDelegate OnNewSong = delegate { };
+        public event NewTrackTimeDelegate OnNewTrackTime = delegate { };
+        public event ServiceConnectedDelegate OnServiceConnected = delegate { };
+        public event ServiceDisconnectedDelegate OnServiceDisconnected = delegate { };
+
+        public void Connect()
+        {
+            
+        }
+
+        public void Disconnect()
+        {
+
+        }
+    }
+}
