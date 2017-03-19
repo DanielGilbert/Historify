dbca63b570613370b35daa91d5257cbc62b8f40f
Daniel Gilbert <ich@danielgilbert.de>
2017-03-19 01:35:34 +0100

    Adds Artist and Album to ViewModel

--- HistoryForSpotify/ViewModels/HistoryItemViewModel.cs
+++ HistoryForSpotify/ViewModels/HistoryItemViewModel.cs
@@ -24,11 +24,39 @@ namespace HistoryForSpotify.ViewModels
             }
         }
 
+        public string Artist
+        {
+            get
+            {
+                return _historyItem.Artist;
+            }
+            set
+            {
+                _historyItem.Artist = value;
+                OnPropertyChanged(nameof(Artist));
+            }
+        }
+
+        public string Album
+        {
+            get
+            {
+                return _historyItem.Album;
+            }
+            set
+            {
+                _historyItem.Album = value;
+                OnPropertyChanged(nameof(Album));
+            }
+        }
+
         public HistoryItemViewModel(HistoryItem historyItem)
         {
             _historyItem = new HistoryItem();
 
             _historyItem.Name = historyItem.Name;
+            _historyItem.Album = historyItem.Album;
+            _historyItem.Artist = historyItem.Artist;
         }
     }
 }
