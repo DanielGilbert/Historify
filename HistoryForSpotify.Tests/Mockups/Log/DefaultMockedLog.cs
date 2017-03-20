90bf09a30c3de01e509f15939288a3f2f537e436
Daniel Gilbert <ich@danielgilbert.de>
2017-03-20 23:41:40 +0100

    Implements a mocked logger

--- HistoryForSpotify.Tests/Mockups/Log/DefaultMockedLog.cs
+++ HistoryForSpotify.Tests/Mockups/Log/DefaultMockedLog.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HistoryForSpotify.Commons.Logging.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,31 @@ using System.Threading.Tasks;
 
 namespace HistoryForSpotify.Tests.Mockups.Log
 {
-    class DefaultMockedLog
+    public class DefaultMockedLog : ILog
     {
+        public void Debug(string message)
+        {
+
+        }
+
+        public void Error(string message)
+        {
+
+        }
+
+        public void Fatal(string message)
+        {
+
+        }
+
+        public void Info(string message)
+        {
+
+        }
+
+        public void Warning(string message)
+        {
+
+        }
     }
 }
