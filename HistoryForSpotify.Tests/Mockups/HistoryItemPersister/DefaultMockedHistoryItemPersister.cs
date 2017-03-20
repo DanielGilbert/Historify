ef4e3bb0581511fc4765b367d9d9a4d7f569eb0b
Daniel Gilbert <ich@danielgilbert.de>
2017-03-20 23:40:43 +0100

    Adds a mocked HistoryItemPersister

--- HistoryForSpotify.Tests/Mockups/HistoryItemPersister/DefaultMockedHistoryItemPersister.cs
+++ HistoryForSpotify.Tests/Mockups/HistoryItemPersister/DefaultMockedHistoryItemPersister.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using HistoryForSpotify.Core.Storage.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HistoryForSpotify.Commons.Models;
 
 namespace HistoryForSpotify.Tests.Mockups.HistoryItemPersister
 {
-    class DefaultMockedHistoryItemPersister
+    class DefaultMockedHistoryItemPersister : IHistoryItemPersister
     {
+        string _data;
+
+        public List<HistoryItem> Load(string saveFolder)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// "Saves" the dummy items
+        /// </summary>
+        /// <param name="historyItems"></param>
+        /// <param name="saveFolder"></param>
+        public void Save(List<HistoryItem> historyItems, string saveFolder)
+        {
+
+        }
     }
 }
